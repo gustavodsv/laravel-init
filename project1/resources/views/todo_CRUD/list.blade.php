7f772ba853440d1b34ab0eb499c82f65c1fa1f17
Gustavo <gustavo-dev@hotmail.com>
2021-12-22 17:46:21 -0300

    adicionando links necessarios para as ações

--- project1/resources/views/todo_CRUD/list.blade.php
+++ project1/resources/views/todo_CRUD/list.blade.php
@@ -6,10 +6,22 @@
 
     @section('header', 'List TO-DO')
 
+    <a href="">[ new ]</a>
+
     @if(count($list) > 0)
-        <ul>
-            @foreach($list as $item)
-                <li>{{$item->title}}</li>
+
+    <ul>
+        @foreach($list as $item)
+            <li>
+                <a href="">[ edit ]</a>
+                <a href="">[ delete ]</a>
+                {{$item->title}}
+                <a href="">
+                    @if($item->status===1) <input type="checkbox" checked>
+                    @else <input type="checkbox">
+                    @endif
+                </a>
+            </li>
             @endforeach
         </ul>
     @else
